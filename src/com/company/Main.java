8eac5a457098ac5347aabe9b0e51e353b94e2f7e
valik98-lab <astafi.valentina@isa.utm.md>
2020-09-12 18:46:52 +0300

    Am adaugat exemple de variabile primitive

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Exemple de variabile
+        int i = 10;
+        long j = 20L;
+        short s = 12480;
+        char c = 'C';
+        float f = 6.F;
+        double d=25D;
+        boolean b=true;
+        byte bit = 22;
     }
 }
